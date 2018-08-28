6867d11e40d536cc52a74d37b9c5936465f82183
袖川航平 <kouhei.sodekawa@gmail.com>
2018-08-29 08:05:00 +0900

    +)通知画面、機能設計完了 -)設定画面から通知画面への遷移

--- Myapp/userSetting/NotificationViewController.swift
+++ Myapp/userSetting/NotificationViewController.swift
@@ -4,6 +4,7 @@ import UserNotifications
 class NotificationViewController: UIViewController {
 
     override func viewDidLoad() {
+     
         super.viewDidLoad()
 
     }
@@ -15,42 +16,58 @@ class NotificationViewController: UIViewController {
     @IBOutlet weak var mySwitch: UISwitch!
     @IBOutlet weak var mydatepicker: UIDatePicker!
     
-    @IBAction func mySwitch(_ sender: UISwitch) {
+    
+    @IBAction func DateSelector(_ sender: UIDatePicker) {
+   
+        changeDate(date: mydatepicker.date)
         
+    }
+    
+
+    @IBAction func mySwitch(_ sender: UISwitch) {
+   
         if mySwitch.isOn {
             mydatepicker.isMultipleTouchEnabled = true
             mydatepicker.isUserInteractionEnabled = true
             
-            let content = UNMutableNotificationContent()
-                content.title = "こころぐを書く時間です"
-                content.body = "今日のこころぐは書きましたか？日々の感情を記録に録りましょう。"
-                content.sound = UNNotificationSound.default()
-            
-            let calendar = NSCalendar(identifier: NSCalendar.Identifier.gregorian)
-            let now = NSDate()
-            let date:NSDateComponents = calendar!.components(NSCalendar.Unit.HourCalendarUnit|NSCalendar.Unit.MinuteCalendarUnit|NSCalendar.Unit.SecondCalendarUnit,fromDate: now)
-            let
-            date.calendar = calendar as! Calendar;
-//                DateComponents(month:7, day:30, hour:17, minute:20)
-            let trigger = UNCalendarNotificationTrigger.init(dateMatching: date as DateComponents, repeats: true)
-            
-            let request = UNNotificationRequest.init(identifier: "CalendarNotification", content: content, trigger: trigger);
-                center.add(request)
         } else {
             mydatepicker.isMultipleTouchEnabled = false
             mydatepicker.isUserInteractionEnabled = false
         }
+    
     }
     
-    internal func changeDate(sender: UIDatePicker){
+        func changeDate(date:Date){
         
+            
         let df = DateFormatter()
-        df.dateFormat = "H時m分"
+        df.dateFormat = "yyyy/MM/dd HH:mm"
         df.locale = Locale(identifier: "ja_JP")
+        let SettingDate = df.string(from: date)
         
-        let SettingDate = df.string(from: sender.date)
-        
+        setNotification(date: SettingDate)
     }
     
+    func setNotification(date:String){
+        
+        let content = UNMutableNotificationContent()
+        content.title = "こころぐを書く時間です"
+        content.body = "今日のこころぐは書きましたか？日々の感情を記録に録りましょう。"
+        content.sound = UNNotificationSound.default()
+        
+        let date = date.components(separatedBy: " ")
+        let dates = date[0].components(separatedBy: "/")
+        let Time = date[1].components(separatedBy: ":")
+    
+        let setDC = DateComponents(year: Int(dates[0]), month: Int(dates[1]), day: Int(dates[2]), hour: Int(Time[0]), minute: Int(Time[1]))
+    
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: setDC as DateComponents, repeats: true)
+        
+        let request = UNNotificationRequest.init(identifier: "CalendarNotification", content: content, trigger: trigger);
+        center.add(request)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    
+    }
 
 }
--- Myapp/userSetting/userSettingTopViewController.swift
+++ Myapp/userSetting/userSettingTopViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import UserNotifications
 
+// セルタップしてから画面遷移
+
 // 設定画面に追加するもの
 // 通知機能,作者ブログ,「こころぐ」のレビュー
 
@@ -9,24 +11,12 @@ var Title = [ ["通知", "通知を設定する"] ]
 class userSettingTopViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var myTableVIew: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myTableVIew.delegate = self
         myTableVIew.dataSource = self
-        
-        let center = UNUserNotificationCenter.current()
-        
-        let content = UNMutableNotificationContent()
-            content.title = "こころぐを書く時間です"
-            content.body = "今日のこころぐは書きましたか？日々の感情を記録に録りましょう。"
-            content.sound = UNNotificationSound.default()
-        
-        let date = DateComponents(month:7, day:30, hour:17, minute:20)
-        let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: true)
-        
-        let request = UNNotificationRequest.init(identifier: "CalendarNotification", content: content, trigger: trigger);          center.add(request)
         
     }
 
@@ -38,7 +28,6 @@ class userSettingTopViewController: UIViewController,UITableViewDelegate,UITable
     func numberOfSections(in tableView: UITableView) -> Int {
         return Title.count
     }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         myTableVIew.backgroundColor = UIColor.yellow
@@ -55,10 +44,12 @@ class userSettingTopViewController: UIViewController,UITableViewDelegate,UITable
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return Title[section][0]
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedData = indexPath.row
-    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let next = storyboard!.instantiateViewController(withIdentifier: "Notification")
+        self.present(next,animated: true, completion: nil)
+        
+        //        performSegue(withIdentifier: "toNotification", sender: nil)
+    }
 
 }
